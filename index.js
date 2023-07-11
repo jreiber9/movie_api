f54f9e027ed92c4fb7563faf7879feb23434556d
jreiber9 <128739362+jreiber9@users.noreply.github.com>
2023-07-11 19:37:33 -0400

    Exericse 2.9 in progress

--- index.js
+++ index.js
@@ -22,76 +22,23 @@ app.use(morgan('combined', {stream: accessLogStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// // user data
-// let users= [
-//   {
-//       id: 1,
-//       name: 'Bill',
-//       favoriteMovies: []
-//   },
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
 
-//   {
-//       id: 2,
-//       name: 'Lauren',
-//       favoriteMovies: ["Interstellar"]
-//   },
 
-//   {
-//       id: 3,
-//       name: 'Tobias',
-//       favoriteMovies: []
-//   },
-    
-// ]
 
-// // top movie data
-// let movies = [
-//   {
-//     Title: 'Star Wars',
-//     Genre: {
-//         Name: 'Adventure',
-//         Description: 'Implies a narrative that is defined by a journey (often including some form of pursuit) and is usually located within a fantasy or exoticized setting.'
-//     },
-//     Description: 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empires world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.',
-//     Director: {
-//         Name: 'George Lucas',
-//         born: 'May 4th, 1944',
-//         bio: 'George Walton Lucas, Jr. is an American film director, producer, and screenwriter.'
-
-//     }
-//   },
-
-//   {
-//     Title: 'Indiana Jones and the Raiders of the Lost Ark',
-//     Genre: {
-//         Name: 'Adventure',
-//         Description: 'Implies a narrative that is defined by a journey (often including some form of pursuit) and is usually located within a fantasy or exoticized setting.'
-//     },
-//     Description: 'In 1936, archaeologist and adventurer Indiana Jones is hired by the U.S. government to find the Ark of the Covenant before the Nazis can obtain its awesome powers.',
-//     Director: {
-//         Name: 'Steven Spielberg',
-//         born: 'December 18th, 1946',
-//         bio: 'Steven Spielberg is an American film director, producer, and screenwriter.'
-
-//     }
-//   },
-
-//   {
-//     Title: 'Interstellar',
-//     Genre: {
-//         Name: 'Sci-Fi',
-//         Description: 'Science fiction (or sci-fi) is a film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, dinosaurs, mutants, interstellar travel, time travel, or other technologies.'
-//     },
-//     Description: 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
-//     Director: {
-//         Name: 'Christopher Nolan',
-//         born: 'July 30th, 1970',
-//         bio: 'Christopher Nolan is an American film director, producer, and screenwriter.'
-
-//     }
-//   },
-
-//   ];
+// GET all movies  
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
 // CREATE Mongoose
 //Add a user
@@ -109,8 +56,7 @@ app.post('/users', (req, res) => {
       if (user) {
         return res.status(400).send(req.body.Username + 'already exists');
       } else {
-        Users
-          .create({
+        Users.create({
             Username: req.body.Username,
             Password: req.body.Password,
             Email: req.body.Email,
@@ -305,112 +251,6 @@ app.delete('/users/:Username', (req, res) => {
 });
 
 
-// Legacy CRUD starts
-
-// UPDATE legacy
-// app.put('/users/:id', (req, res) => {
-//   const { id } = req.params;
-//   const updatedUser = req.body;
-
-//   let user = users.find( user => user.id == id);
-
-//   if (user) {
-//       user.name = updatedUser.name;
-//       res.status(200).json(user);
-//   } else {
-//       res.status(400).send('users need names')
-//   }
-// });
-
-// // CREATE legacy
-// app.post('/users/:id/:movieTitle', (req, res) => {
-//   const { id, movieTitle } = req.params;
-
-//   let user = users.find( user => user.id == id);
-
-//   if (user) {
-//       user.favoriteMovies.push(movieTitle);
-//       res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;
-//   } else {
-//       res.status(400).send('no such user')
-//   }
-// });
-
-// // DELETE legacy
-// app.delete('/users/:id/:movieTitle', (req, res) => {
-//   const { id, movieTitle } = req.params;
-
-//   let user = users.find( user => user.id == id);
-
-//   if (user) {
-//       user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-//       res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;
-//   } else {
-//       res.status(400).send('no such user')
-//   }
-// });
-
-// // DELETE legacy
-// app.delete('/users/:id', (req, res) => {
-//   const { id, movieTitle } = req.params;
-
-//   let user = users.find( user => user.id == id);
-
-//   if (user) {
-//       user= users.filter( user => user.id != id);
-//       res.status(200).send(`user ${id} has been deleted`);
-//   } else {
-//       res.status(400).send('no such user')
-//   }
-// });
-
-// // READ legacy
-// app.get('/movies', (req,res) => {
-//   res.status(200).json(movies);
-// });
-
-// // READ legacy
-// app.get('/movies/:title', (req,res) => {
-//   const { title } = req.params;
-//   const movie = movies.find( movie => movie.Title === title );
-
-//   if (movie) {
-//     res.status(200).json(movie);
-//   } else {
-//       res.status(400).send('no such movie')
-//   }
-
-// });
-
-// // READ legacy
-// app.get('/movies/genre/:genreName', (req,res) => {
-//   const { genreName } = req.params;
-//   const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
-
-//   if (genre) {
-//     res.status(200).json(genre);
-//   } else {
-//       res.status(400).send('no such genre')
-//   }
-
-// });
-
-// // READ legacy
-// app.get('/movies/director/:directorName', (req,res) => {
-//   const { directorName } = req.params;
-//   const director = movies.find( movie => movie.Director.Name === directorName ).Director;
-
-//   if (director) {
-//     res.status(200).json(director);
-//   } else {
-//       res.status(400).send('no such director')
-//   }
-
-// });
-
-// Legacy CRUD ends
-
-
   // serving static files
   app.use(express.static('public'));
   
@@ -421,10 +261,6 @@ app.delete('/users/:Username', (req, res) => {
   
   app.get('/documentation', (req, res) => {                  
     res.sendFile('public/documentation.html', { root: __dirname });
-  });
-  
-  app.get('/movies', (req, res) => {
-    res.json(movies);
   });
 
   // error handling
